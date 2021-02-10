e418d1749e7a8545340e5e2be4c3e1f4a0f17b89
seungjae9 <moseung18@gmail.com>
2021-02-10 18:34:00 +0900

    vue

--- test/src/views/Home.vue
+++ test/src/views/Home.vue
@@ -7,6 +7,23 @@
     <input type="text" v-model="input1">
     <button type="button" @click="getData">Get</button>
     <button type="button" @click="setData">set</button>
+    <!--change써가지고 셀렉트박스 이벤트 만들 수 있음-->
+    <select class="form-control" v-model="region" @change="changeRegion">
+      <!--for문 돌릴때는 꼭 vbind key를 입력해줘야한다.-->
+      <!--key란 유일한 값(idx)-->
+      <option :key="idx" :value="data.v" v-for="(data, idx) in options">
+        {{ data.t }}
+      </option>
+    </select>
+    <!--v-if는 조건에 따라서 실제 렌더링 할지 말지를 결정한다.-->
+    <!--v-show는 조건이 만족하든 안하든 렌더링은 하는데 display none 처리 해버린다.(렌더링하지만 안보이게 만든다.)-->
+    <!--자주로 보였다 안보였다 할거면 v-show 처리하는게 낫다.(리소스의 문제)-->
+    <table class="table table-bordered" v-if="tableShow">
+      <tr :key="idx" v-for="(data, idx) in options">
+        <td>{{ data.v }}</td>
+        <td>{{ data.t }}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -17,6 +34,13 @@ export default {
       title: '박승재',
       title2: '광주',
       input1: 'abed',
+      options: [
+        {v: "S", t: "seoul"},
+        {v: "J", t: "jeju"},
+        {v: "B", t: "busan"},
+      ],
+      region: "B",
+      tableShow: false
     };
   },
   // watch 내에서 정의한 데이터가 바뀌면 와치가 캐치해서 알 수 있다.
@@ -33,6 +57,9 @@ export default {
     },
     setData() {
       this.input1 = '1234';
+    },
+    changeRegion() {
+      alert(this.region);
     }
   },
 
